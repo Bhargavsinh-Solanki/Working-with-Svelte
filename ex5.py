9f814bda46cd179c1d432f853a9d350be5dff5ed
Bhargavsinh Solanki <bhargavsinh.solanki@uni-konstanz.de>
2023-07-09 13:05:41 +0200

    more variables

---
+++ ex5.py
@@ -0,0 +1,20 @@
+# MORE VARIABLES AND PRINTING
+
+my_name = 'Bhargav Solanki'
+my_age = 25 #not a lie
+my_height = 177 #cm
+my_weight = 74 #kg
+my_eyes = 'brown'
+my_teeth = 'White'
+my_hair = 'black'
+
+print(f"Lets talk about my {my_name}")
+print(f"He is {my_height} centimeters tall")
+print(f"He is {my_weight} kilograms heavy")
+print("Actually thats not too heavy")
+print(f"He has got {my_eyes} eyes and {my_hair} hair")
+print(f"His teeth are usually {my_teeth} depending on the coffee")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
